b2b90e2ec51ec1fbf1a8bb21aedde87333004722
sakattack85 <viktorat85@gmail.com>
2021-02-03 20:11:16 +0200

    bb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'showforms';
+ user=new User('','','','','');
+  retrievedata(formdata){
+this.user=formdata;
+  }
 }
